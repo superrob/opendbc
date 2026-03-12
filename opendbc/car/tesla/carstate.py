f44f796a446214d138010e4821b151980ee4f3ae
dzid26 <dzidmail@gmail.com>
2026-03-12 22:43:43 +0000

    Stalkless highland support (#3)
    * tesla; add cruiseCancel variable
    * watch OEM DAS_control for OP long cancelling, as the car can see button presses.
    
    * revert OEM DAS_accState trigger
    
    * tesla; parsing scrollWheelPressed with DAS_accState
    * raise openpilot buttonEvent ButtonType.cancel
    
    * tesla; raise ButtonType cancel condition on DAS_accState == 13
    
    ---------
    
    Co-authored-by: Curtis Jenkins <curtis.x.jenkins@gmail.com>
    (cherry picked from commit 5d82d12ab41a4974abaa4d7b6ff2dda03ebf2297)

--- opendbc/car/tesla/carstate.py
+++ opendbc/car/tesla/carstate.py
@@ -1,6 +1,6 @@
 import copy
 from opendbc.can import CANDefine, CANParser
-from opendbc.car import Bus, structs
+from opendbc.car import Bus, create_button_events, structs
 from opendbc.car.carlog import carlog
 from opendbc.car.common.conversions import Conversions as CV
 from opendbc.car.interfaces import CarStateBase
@@ -28,6 +28,7 @@ class CarState(CarStateBase, CarStateExt):
     self.suspected_fsd14 = False
 
     self.hands_on_level = 0
+    self.prev_acc_state = 0
     self.das_control = None
 
   def update_summon_state(self, summon_state: str, cruise_enabled: bool):
@@ -107,6 +108,16 @@ class CarState(CarStateBase, CarStateExt):
     ret.cruiseState.standstill = False  # This needs to be false, since we can resume from stop without sending anything special
     ret.standstill = cp_party.vl["ESP_B"]["ESP_vehicleStandstillSts"] == 1
     ret.accFaulted = cruise_state == "FAULT"
+
+    acc_state = cp_ap_party.vl["DAS_control"]["DAS_accState"]
+    ret.buttonEvents = [
+      *create_button_events(
+        acc_state,
+        self.prev_acc_state,
+        {13: ButtonType.cancel},
+      ),
+    ]
+    self.prev_acc_state = acc_state
 
     # Gear
     ret.gearShifter = GEAR_MAP[self.can_define.dv["DI_systemStatus"]["DI_gear"].get(int(cp_party.vl["DI_systemStatus"]["DI_gear"]), "DI_GEAR_INVALID")]
